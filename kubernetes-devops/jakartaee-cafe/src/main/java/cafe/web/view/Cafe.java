bb951431ecd48d5ff454e3f2711682e6054dd55c
Reza Rahman <reza_rahman@lycos.com>
2021-02-23 21:10:34 -0500

    Update Cafe.java

--- kubernetes-devops/jakartaee-cafe/src/main/java/cafe/web/view/Cafe.java
+++ kubernetes-devops/jakartaee-cafe/src/main/java/cafe/web/view/Cafe.java
@@ -2,16 +2,12 @@ package cafe.web.view;
 
 import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.WebApplicationException;
@@ -31,7 +27,7 @@ public class Cafe implements Serializable {
   private static final Logger logger =
       Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
-  private String baseUri;
+  private String SERVICE_URI = "http://localhost:9080/jakartaee-cafe/rest/coffees";
   private transient Client client;
 
   @NotNull @NotEmpty protected String name;
@@ -61,25 +57,9 @@ public class Cafe implements Serializable {
   @PostConstruct
   private void init() {
     try {
-      InetAddress inetAddress =
-          InetAddress.getByName(
-              ((HttpServletRequest)
-                      FacesContext.getCurrentInstance().getExternalContext().getRequest())
-                  .getServerName());
-
-      baseUri =
-          FacesContext.getCurrentInstance().getExternalContext().getRequestScheme()
-              + "://"
-              + inetAddress.getHostName()
-              + ":"
-              + FacesContext.getCurrentInstance().getExternalContext().getRequestServerPort()
-              + "/jakartaee-cafe/rest/coffees";
       this.client = ClientBuilder.newClient();
       this.getAllCoffees();
-    } catch (IllegalArgumentException
-        | NullPointerException
-        | WebApplicationException
-        | UnknownHostException ex) {
+    } catch (IllegalArgumentException | NullPointerException | WebApplicationException ex) {
       logger.severe("Processing of HTTP response failed.");
       ex.printStackTrace();
     }
@@ -88,22 +68,21 @@ public class Cafe implements Serializable {
   private void getAllCoffees() {
     this.coffeeList =
         this.client
-            .target(this.baseUri)
-            .path("/")
+            .target(this.SERVICE_URI)
             .request(MediaType.APPLICATION_JSON)
             .get(new GenericType<List<Coffee>>() {});
   }
 
   public void addCoffee() {
     Coffee coffee = new Coffee(this.name, this.price);
-    this.client.target(baseUri).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
+    this.client.target(SERVICE_URI).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
     this.name = null;
     this.price = null;
     this.getAllCoffees();
   }
 
   public void removeCoffee(String coffeeId) {
-    this.client.target(baseUri).path(coffeeId).request().delete();
+    this.client.target(SERVICE_URI).path(coffeeId).request().delete();
     this.getAllCoffees();
   }
 }
